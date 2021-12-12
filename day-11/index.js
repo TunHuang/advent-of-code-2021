e08bac547a96c3e8e6cef02c6d64fbd5601a7bcc
Tun Huang <huang.tun.anton@gmail.com>
2021-12-12 06:38:59 +0100

    day 11 uses array instead of string for coordinates

--- day-11/index.js
+++ day-11/index.js
@@ -2,37 +2,35 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString();
 const inputArray = input.split('\n').map(row => row.split('').map(char => +char));
 
-const mkCor = (y, x) => y + '-' + x;
-
-function getAdjacents(array, y, x) {
+function getAdjacents(array, [y, x]) {
   const adjacents = [];
   const top = y > 0;
   const right = x < array[0].length - 1;
   const bottom = y < array.length - 1;
   const left = x > 0;
   if (top) {
-    adjacents.push(mkCor(y - 1, x));
+    adjacents.push([y - 1, x]);
   }
   if (top && right) {
-    adjacents.push(mkCor(y - 1, x + 1));
+    adjacents.push([y - 1, x + 1]);
   }
   if (right) {
-    adjacents.push(mkCor(y, x + 1));
+    adjacents.push([y, x + 1]);
   }
   if (bottom && right) {
-    adjacents.push(mkCor(y + 1, x + 1));
+    adjacents.push([y + 1, x + 1]);
   }
   if (bottom) {
-    adjacents.push(mkCor(y + 1, x));
+    adjacents.push([y + 1, x]);
   }
   if (bottom && left) {
-    adjacents.push(mkCor(y + 1, x - 1));
+    adjacents.push([y + 1, x - 1]);
   }
   if (left) {
-    adjacents.push(mkCor(y, x - 1));
+    adjacents.push([y, x - 1]);
   }
   if (top && left) {
-    adjacents.push(mkCor(y - 1, x - 1));
+    adjacents.push([y - 1, x - 1]);
   }
   return adjacents;
 }
@@ -44,13 +42,13 @@ function energyStep(array) {
     for (let x = 0; x < array[0].length; x++) {
       array[y][x]++;
       if (array[y][x] > 9) {
-        flashingOctopus.push(mkCor(y, x));
+        flashingOctopus.push([y, x]);
       }
     }
   }
 
   flashingRecursive(array, flashingOctopus);
-  
+
   let flashCounter = 0;
   for (let y = 0; y < array.length; y++) {
     for (let x = 0; x < array[y].length; x++) {
@@ -63,25 +61,22 @@ function energyStep(array) {
   return flashCounter;
 }
 
-
 function flashingRecursive(array, flashArray) {
   const newFlashingOctopus = [];
   flashArray.forEach(cor => {
-    const [y, x] = cor.split('-').map(value => +value);
-    newFlashingOctopus.push(...flash(array, y, x));
+    newFlashingOctopus.push(...flash(array, cor));
   });
   if (newFlashingOctopus.length > 0) {
     flashingRecursive(array, newFlashingOctopus);
   }
 }
 
-function flash(array, y, x) {
-  const adjacents = getAdjacents(array, y, x);
+function flash(array, cor) {
+  const adjacents = getAdjacents(array, cor);
   const newFlashingOctopus = [];
-  adjacents.forEach(cor => {
-    const [y, x] = cor.split('-').map(value => +value);
+  adjacents.forEach(([y, x]) => {
     if (array[y][x] === 9) {
-      newFlashingOctopus.push(cor);
+      newFlashingOctopus.push([y, x]);
     }
     array[y][x]++;
   });
